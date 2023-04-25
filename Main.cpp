b002361abc389bb92500841d9a545930c5380125
Dominik Rutter <dominik.rutter@gmail.com>
2023-04-24 22:35:13 -0400

    Improve efficiency and readability of bubblesort code

--- Main.cpp
+++ Main.cpp
@@ -23,29 +23,42 @@ int main()
 	const int dataSetSize = 20;
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 
-	for (int i = 0; i < dataSetSize; i++) 
+	for (int i = 0; i < dataSetSize; i++)
 	{
 		numberArray.push_back(i);
 	}
 
+	
 	anna::displayData(numberArray);
 
 	std::shuffle(numberArray.begin(), numberArray.end(), std::default_random_engine(seed));
 
 	anna::displayData(numberArray);
-	
-	for (int i = 0; i < numberArray.size() - 1; i++) 
+	/*
+	start at beginning of array
+	move towards reducing size end
+	swap largest element, to front
+	now sorted front can be ignored
+	*/
+
+	for (int arrayEnd = numberArray.size() - 1; arrayEnd > 0; arrayEnd--)
 	{
-		for (int j = i + 1; j < numberArray.size(); j++)
+		static int swaps = 0;
+		static int checks = 0;
+		bool sorted = true;
+		for (int arrayScan = 0; arrayScan < numberArray.size() - 1; arrayScan++)
 		{
-			if (numberArray[i] > numberArray[j]) {
-				int swap;
-				swap = numberArray[j];
-				numberArray[j] = numberArray[i];
-				numberArray[i] = swap;
+			checks++;
+			if (numberArray[arrayScan] > numberArray[arrayScan + 1])
+			{
+				swaps++;
+				sorted = false;
+				std::swap(numberArray[arrayScan], numberArray[arrayScan + 1]);
 				anna::displayData(numberArray);
 			}
 		}
+		//std::wcout << L"Checks :" << std::to_wstring(checks) << L"  Swaps :" << std::to_wstring(swaps) << L"\n";
+		if (sorted) break;
 	}
 
 	anna::displayData(numberArray);

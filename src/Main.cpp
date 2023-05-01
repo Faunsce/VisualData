61370ac9a14e73886eec9ecae22afe5da23941b3
Dominik Rutter <dominik.rutter@gmail.com>
2023-04-30 20:33:13 -0400

    Improve splash and allow dynamic array size.

--- src/Main.cpp
+++ src/Main.cpp
@@ -19,32 +19,60 @@ int main()
 	std::random_device randomSeed{}; 
 	std::mt19937 randomEngine{ randomSeed() };
 
+	std::wcout << L"\x1b[2J\x1b[H";
 	std::wcout
-		<< L"╔══════════════════════════════════════════════════════════════════════════════════════════════════╗\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                              Welcome to my little program!                                       ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"║                                                                                                  ║\n"
-		<< L"╚══════════════════════════════════════════════════════════════════════════════════════════════════╝\n"
-		<< L"\x1b[2J\x1b[H";
+		<< L"╔═════════════════════════════════════════════════════════════════╗\n"
+		<< L"║                                                                 ║\n"
+		<< L"║                Welcome to my little program!                    ║\n"
+		<< L"║                                                                 ║\n"
+		<< L"║                                                                 ║\n"
+		<< L"║             In this program you will be able to                 ║\n"
+		<< L"║             set a data array to a fixed length,                 ║\n"
+		<< L"║             shuffle it, then re-organize it                     ║\n"
+		<< L"║             using an algorithm of your choice!                  ║\n"
+		<< L"║                                                                 ║\n"
+		<< L"║                                                                 ║\n"
+		<< L"║                                                                 ║\n"
+		<< L"║                                                                 ║\n"
+		<< L"║                                                                 ║\n"
+		<< L"╚═════════════════════════════════════════════════════════════════╝\n";
+	std::this_thread::sleep_for(std::chrono::seconds(5));
 	
-	std::this_thread::sleep_for(std::chrono::seconds(3));
+	std::wstring userInput;
+	bool clean;
+	do {
+		clean = true;
+		std::wcout << L"\x1b[2J\x1b[H";
+		std::wcout
+			<< L"╔═════════════════════════════════════════════════════════════════╗\n"
+			<< L"║                                                                 ║\n"
+			<< L"║            Please enter set the size of your data!              ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║             You may enter a value between 10 and                ║\n"
+			<< L"║             100 for the size of the array.                      ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"║                                                                 ║\n"
+			<< L"╚═════════════════════════════════════════════════════════════════╝\n"
+			<< L"Size of array : ";
+		std::getline(std::wcin, userInput);
+		for (const auto& value : userInput)
+		{
+			if (!std::isdigit(value))
+			{
+				clean = false;
+				break;
+			}
+		}
+	} while (!clean);
+
 	std::vector<int> numberArray;
-	const int dataSetSize = 20;
+	int dataSetSize = std::stoi(userInput);
 	for (int i = 1; i <= dataSetSize; i++)
 	{
 		numberArray.push_back(i);
